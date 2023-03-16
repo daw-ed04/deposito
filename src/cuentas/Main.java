1f911be312fda0598b884dcdb38866f4603a04a5
riffraff <riffraff@trasto>
2023-03-16 18:32:37 +0100

    Primer commit

---
+++ src/cuentas/Main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package cuentas;
+
+import cuentas.CCuenta;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta miCuenta;
+        double saldoActual;
+
+        operativa_cuenta(null);
+    }
+
+    private static void operativa_cuenta(Float cantidad) {
+        CCuenta miCuenta;
+        double saldoActual;
+        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = miCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        try {
+            miCuenta.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            miCuenta.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
